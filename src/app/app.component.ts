91bce1178df31283ecf911d0c421d8891210725f
l3kica <nisamdejan@gmail.com>
2024-08-13 00:20:32 +0200

    Dodata lista narudzbina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import AOS from 'aos';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+import { CommonModule } from '@angular/common';  
+import { BrowserModule } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatBadgeModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Cool-site';
   listoforders: string[] = []
-  
+  hidden = false;
+  basketOpen: boolean = false
+  addToList(value: string)
+  {
+    this.listoforders.push(value)
+    console.log(this.listoforders.length)
+  }
+  toggleBadgeVisibility() {
+    this.hidden = !this.hidden;
+  }
+
+  toggleBasket(){
+    this.basketOpen = !this.basketOpen
+  }
 
   scrollUp(){
     window.scrollTo({
@@ -25,4 +45,5 @@ export class AppComponent {
   {
     AOS.init();
   }
+
 }
